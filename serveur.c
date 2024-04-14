010f67efdfbe7c6e4fe70ba0c08e5419fae309dc
Yohann.Schatt <yohann.schatt@etu.umontpellier.fr>
2024-04-14 19:03:46 +0200

    ajout des mutex

--- serveur.c
+++ serveur.c
@@ -7,29 +7,32 @@
 #include <stdbool.h>
 #include <pthread.h>
 
-int NB_MAX_PERSONNE = 100;
+#define NB_MAX_PERSONNE 100
+int NB_PERSONNE_ACTUELLE = 0;
+
+pthread_mutex_t M1 = PTHREAD_MUTEX_INITIALIZER;
+
+int tabdSC[NB_MAX_PERSONNE];
 
 struct Args_Thread {
-    int id_tab;
+    int id;
     int dSC;
-    int* dSC_autre;
 };
 
-void fin_connexion(int dSC,int **dSC_autre,int id) {
-    *dSC_autre[id] = -1;
-    shutdown(dSC,2) ;
+void fin_connexion(int dSC,int id) {
+    pthread_mutex_lock(&M1);
+    tabdSC[id] = -1;
+    NB_PERSONNE_ACTUELLE--;
+    pthread_mutex_unlock(&M1);
+    shutdown(dSC,2);
     printf("fermeture\n");
 }
 
-bool lecture(int dSC,char **msg, int id){
-    printf("je suis dans lecture\n");
+bool lecture(int dSC,char **msg){
     bool res = true;
     int taille;
-    printf("dSC%d : %d\n",id,dSC);
     recv(dSC,&taille, sizeof(int), 0);
-    printf("coucou");
     recv(dSC, *msg, taille, 0);
-    printf("message de %d : %s\n",id,*msg);
     if((strcmp(*msg,"fin") == 0)){
         res = false;
     }
@@ -47,20 +50,21 @@ void* lecture_envoie(void* args_thread) {
     struct Args_Thread args = *((struct Args_Thread*)args_thread);
     char* msg = (char*)malloc(128*sizeof(char));
     bool continu = true;
-    int id_tab = args.id_tab;
+    int id = args.id;
     int dSC = args.dSC;
-    int* dSC_autre = args.dSC_autre;
     while (continu) {
-        continu = lecture(dSC, &msg,id_tab);
+        continu = lecture(dSC, &msg);
         if (continu){
             for(int i = 0;i<NB_MAX_PERSONNE;i++) {
-                if (dSC != dSC_autre[i] && dSC_autre[i] != -1) {
-                    envoie(dSC_autre[i], &msg);  
+                pthread_mutex_lock(&M1);
+                if (tabdSC[i] != -1 && dSC != tabdSC[i]) {
+                    envoie(dSC, &msg);  
                 }
+                pthread_mutex_unlock(&M1);
             }
         }
     }
-    fin_connexion(args.dSC,&args.dSC_autre,args.id_tab);
+    fin_connexion(dSC,id);
 }
 
 int init_ouverture_connexion(int port) {
@@ -83,8 +87,6 @@ int init_ouverture_connexion(int port) {
 
 void init_connexion(int dS,int* tabdSC, pthread_t* thread) {
 
-    printf("je suis dans init_connexion\n");
-
     int count = 0;
 
     for(int i = 0; i<NB_MAX_PERSONNE;i++) {
@@ -92,30 +94,34 @@ void init_connexion(int dS,int* tabdSC, pthread_t* thread) {
     }
     int i = 0;
 
+    struct sockaddr_in aC ;
+    socklen_t lg = sizeof(struct sockaddr_in) ;
+
     while(true){
 
-        struct sockaddr_in aC ;
-        socklen_t lg = sizeof(struct sockaddr_in) ;
-        if (count < NB_MAX_PERSONNE){
-            printf("j'attend l'acceptation\n");
-            int dSC = accept(dS, (struct sockaddr*) &aC,&lg) ;
+        int dSC = accept(dS, (struct sockaddr*) &aC,&lg) ;
+        
+        if (count < NB_MAX_PERSONNE-1){
             printf("Client Connecté\n");
 
+            pthread_mutex_lock(&M1);
             while(tabdSC[i] != -1){
                 i = (i + 1)%NB_MAX_PERSONNE;
             }
             tabdSC[i] = dSC;
+            NB_PERSONNE_ACTUELLE++;
             printf("dSC%d : %d\n",i,dSC);
 
-            struct Args_Thread args;
-            args.id_tab = i;
-            args.dSC = dSC;
-            args.dSC_autre = tabdSC;
+            pthread_mutex_unlock(&M1);
 
-            printf("je crée un thread avec l'id : %d\n",i);
+            struct Args_Thread args;
+            args.id = i;
+            args.dSC = dSC;
 
             pthread_create(&thread[i], NULL, lecture_envoie,(void*)&args);
-            count++;
+        }
+        else {
+            shutdown(dSC,2);
         }
     }
 }
@@ -128,14 +134,7 @@ int main(int argc, char *argv[]) {
 
     int dS = init_ouverture_connexion(atoi(argv[1]));
 
-    char* msg = malloc(128*sizeof(char));
-
-    int* tabdSC = (int*)malloc(NB_MAX_PERSONNE*sizeof(int));
-
     init_connexion(dS,tabdSC,thread);
-
-    free(tabdSC);
-    free(msg);
     shutdown(dS,2);
     printf("fin programme");
 }
