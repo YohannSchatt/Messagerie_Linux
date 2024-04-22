eaceab946cd1238f89f2f8c4f60a79a2f3e0731d
BARBEDET <anthony@MacBook-Pro-de-BARBEDET.local>
2024-04-22 15:27:25 +0200

    ant blablabalar

--- client.c
+++ client.c
@@ -5,74 +5,69 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <errno.h> // Pour errno et perror
 
-void fin(int dS,char** msg) {
-    shutdown(dS,2) ;
-    free(*msg);
-    printf("Fin du programme");
+void handle_error(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
 }
 
-bool lecture(int dS, char** msg){
+void fin(int dS, char **msg) {
+    if (shutdown(dS, 2) == -1) {
+        handle_error("shutdown");
+    }
+    free(*msg);
+    printf("Fin du programme\n");
+}
+
+bool lecture(int dS, char **msg) {
     bool res = true;
     int taille;
-    recv(dS,&taille, sizeof(int), 0);
-    recv(dS, *msg,128+1, 0);
-    if((strcmp(*msg,"fin") == 0)){
+    if (recv(dS, &taille, sizeof(int), 0) == -1) {
+        handle_error("recv");
+    }
+    if (recv(dS, *msg, taille, 0) == -1) {
+        handle_error("recv");
+    }
+    if ((strcmp(*msg, "fin") == 0)) {
         res = false;
     }
-    printf("L'autre utilisateur dit : %s\n",*msg);
+    printf("L'autre utilisateur dit : %s\n", *msg);
     return res;
 }
 
-bool envoie(int dS, char** msg){
+bool envoie(int dS, char **msg) {
     bool res = true;
     printf("Ecrit un message : ");
-    fgets(*msg,128,stdin);
-    char *pos = strchr(*msg,'\n');
+    fgets(*msg, 128, stdin);
+    char *pos = strchr(*msg, '\n');
     *pos = '\0';
-    if((strcmp(*msg,"fin") == 0)){
+    if ((strcmp(*msg, "fin") == 0)) {
         res = false;
     }
-    int taille = strlen(*msg)+1;
-    send(dS, &taille, sizeof(int), 0);
-    send(dS, *msg, taille, 0);
+    int taille = strlen(*msg) + 1;
+    if (send(dS, &taille, sizeof(int), 0) == -1) {
+        handle_error("send");
+    }
+    if (send(dS, *msg, taille, 0) == -1) {
+        handle_error("send");
+    }
     return res;
 }
 
-int main(int argc, char* argv[]){
-
+int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("./client IP Port");
+        printf("Usage: %s <IP> <Port>\n", argv[0]);
+        exit(EXIT_FAILURE);
     }
-    else{
-        printf("Début programme\n");
-        int dS = socket(PF_INET, SOCK_STREAM, 0);
-        printf("Socket Créé\n");
 
-        struct sockaddr_in aS;
-        aS.sin_family = AF_INET;
-        inet_pton(AF_INET,argv[1],&(aS.sin_addr)) ;
-        aS.sin_port = htons(atoi(argv[2])) ;
-        socklen_t lgA = sizeof(struct sockaddr_in) ;
-        connect(dS, (struct sockaddr *) &aS, lgA) ;
-        printf("Socket Connecté\n");
-
-        bool continu = true;
-        int pos;
-
-        recv(dS, &pos, sizeof(int), 0);
-
-        char* msg = malloc(128*sizeof(char));
-
-        while(continu){
-            if (pos == 1) {
-                continu = envoie(dS,&msg);
-            }
-            else {
-                continu = lecture(dS,&msg); 
-            }
-            pos = (pos+1)%2;
-        }
-        fin(dS,&msg);
+    printf("Début programme\n");
+    int dS = socket(PF_INET, SOCK_STREAM, 0);
+    if (dS == -1) {
+        handle_error("socket");
     }
-}
+    printf("Socket Créé\n");
+
+    struct sockaddr_in aS;
+    aS.sin_family
+
--- serveur.c
+++ serveur.c
@@ -5,100 +5,136 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <errno.h> // Pour errno et perror
 
-void fin_connexion(int dSC) {
-    shutdown(dSC,2) ;
-    printf("fermeture\n");
+void handle_error(const char *msg) {
+    perror(msg);
+    exit(EXIT_FAILURE);
 }
 
-bool lecture(int dSC,char **msg){
+void fin_connexion(int dSC) {
+    if (shutdown(dSC, 2) == -1) {
+        handle_error("shutdown");
+    }
+    printf("Fermeture\n");
+}
+
+bool lecture(int dSC, char **msg) {
     bool res = true;
     int taille;
-    recv(dSC,&taille, sizeof(int), 0);
-    recv(dSC, *msg, taille, 0);
-    if((strcmp(*msg,"fin") == 0)){
+    if (recv(dSC, &taille, sizeof(int), 0) == -1) {
+        handle_error("recv");
+    }
+    if (recv(dSC, *msg, taille, 0) == -1) {
+        handle_error("recv");
+    }
+    if ((strcmp(*msg, "fin") == 0)) {
         res = false;
     }
-    printf("%s\n",*msg);
+    printf("%s\n", *msg);
     return res;
 }
 
-void envoie(int dSC,char** msg){
-    int taille = strlen(*msg)+1;
-    send(dSC, &taille, sizeof(int), 0);
-    send(dSC, *msg, taille , 0);
+void envoie(int dSC, char **msg) {
+    int taille = strlen(*msg) + 1;
+    if (send(dSC, &taille, sizeof(int), 0) == -1) {
+        handle_error("send");
+    }
+    if (send(dSC, *msg, taille, 0) == -1) {
+        handle_error("send");
+    }
 }
 
 int init_ouverture_connexion(int port) {
-    
     int dS = socket(PF_INET, SOCK_STREAM, 0);
+    if (dS == -1) {
+        handle_error("socket");
+    }
     printf("Socket Créé\n");
 
     struct sockaddr_in ad;
     ad.sin_family = AF_INET;
-    ad.sin_addr.s_addr = INADDR_ANY ;
-    ad.sin_port = htons(port) ;
-    bind(dS, (struct sockaddr*)&ad, sizeof(ad)) ;
+    ad.sin_addr.s_addr = INADDR_ANY;
+    ad.sin_port = htons(port);
+    if (bind(dS, (struct sockaddr *)&ad, sizeof(ad)) == -1) {
+        handle_error("bind");
+    }
     printf("Socket Nommé\n");
 
-    listen(dS, 7);
+    if (listen(dS, 7) == -1) {
+        handle_error("listen");
+    }
     printf("Mode écoute\n");
 
     return dS;
 }
 
-int* init_connexion(int dS) {
+int *init_connexion(int dS) {
+    printf("Je suis dans init_connexion\n");
 
-    printf("je suis dans init_connexion\n");
-
-    struct sockaddr_in aC ;
-    socklen_t lg1 = sizeof(struct sockaddr_in) ;
-    int dSC1 = accept(dS, (struct sockaddr*) &aC,&lg1) ;
+    struct sockaddr_in aC;
+    socklen_t lg1 = sizeof(struct sockaddr_in);
+    int dSC1 = accept(dS, (struct sockaddr *)&aC, &lg1);
+    if (dSC1 == -1) {
+        handle_error("accept");
+    }
     printf("Client 1 Connecté\n");
 
-    struct sockaddr_in aB ;
-    socklen_t lg2 = sizeof(struct sockaddr_in) ;
-    int dSC2 = accept(dS, (struct sockaddr*) &aB,&lg2) ;
+    struct sockaddr_in aB;
+    socklen_t lg2 = sizeof(struct sockaddr_in);
+    int dSC2 = accept(dS, (struct sockaddr *)&aB, &lg2);
+    if (dSC2 == -1) {
+        handle_error("accept");
+    }
     printf("Client 2 Connecté\n");
 
     int r1 = 0;
     int r2 = 1;
 
-    send(dSC1, &r1, sizeof(int), 0) ;
+    if (send(dSC1, &r1, sizeof(int), 0) == -1) {
+        handle_error("send");
+    }
 
-    send(dSC2, &r2, sizeof(int), 0) ;
+    if (send(dSC2, &r2, sizeof(int), 0) == -1) {
+        handle_error("send");
+    }
 
-    int* tabdSC = malloc(2*sizeof(int));
+    int *tabdSC = malloc(2 * sizeof(int));
     tabdSC[0] = dSC1;
-    tabdSC[1] = dSC2; 
+    tabdSC[1] = dSC2;
     return tabdSC;
 }
 
 int main(int argc, char *argv[]) {
-  
+    if (argc != 2) {
+        printf("Usage: %s <port>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     printf("Début programme\n");
 
     int dS = init_ouverture_connexion(atoi(argv[1]));
 
-    char* msg = malloc(128*sizeof(char));
+    char *msg = malloc(128 * sizeof(char));
 
-    while(true){
+    while (true) {
+        int *tabdSC = init_connexion(dS);
 
-        int* tabdSC = init_connexion(dS);
-    
         bool continu = true;
         int pos = 1;
 
-        while(continu){
-            continu = lecture(tabdSC[pos],&msg);
-            envoie(tabdSC[(pos+1)%2],&msg);
-            pos = (pos+1)%2;
+        while (continu) {
+            continu = lecture(tabdSC[pos], &msg);
+            envoie(tabdSC[(pos + 1) % 2], &msg);
+            pos = (pos + 1) % 2;
         }
         fin_connexion(tabdSC[0]);
         fin_connexion(tabdSC[1]);
         free(tabdSC);
     }
     free(msg);
-    shutdown(dS,2);
-    printf("fin programme");
+    if (shutdown(dS, 2) == -1) {
+        handle_error("shutdown");
+    }
+    printf("Fin programme\n");
 }
